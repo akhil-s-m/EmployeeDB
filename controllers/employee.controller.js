d0d1b1c99b87398f34269f76dc7d8055fb3c8c50
Akhil Sharma <123203483+akhil-s-m@users.noreply.github.com>
2026-04-06 12:53:05 +0530

    Updated logging

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -5,19 +5,23 @@ const createEmployee = async (req, res) => {
     res
       .status(200)
       .json({ message: "Employee created successfully", employee: employee });
+    console.log(employee + " Added");
   } catch (error) {
     console.error(error);
 
     res.status(500).json({ error: error.message });
+    console.log("Failed to add employee");
   }
 };
 const getAllEmployees = async (req, res) => {
   try {
     const employees = await Employee.find({});
     res.status(200).json(employees);
+    console.log("Fetched all employees");
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
+    console.log("Failed to fetch employees");
   }
 };
 const getEmployeeById = async (req, res) => {
@@ -27,11 +31,12 @@ const getEmployeeById = async (req, res) => {
       return res.status(404).json({ message: "Employee not found" });
     } else {
       res.status(200).json(employee);
-      console.log(employee);
+      console.log(employee + " Fetched");
     }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
+    console.log("Failed to fetch employee");
   }
 };
 
@@ -42,10 +47,12 @@ const deleteEmployee = async (req, res) => {
       return res.status(404).json({ message: "Employee not found" });
     } else {
       res.status(200).json({ message: "Employee deleted successfully" });
+      console.log(employee + " Deleted");
     }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
+    console.log("Failed to delete employee");
   }
 };
 const updateEmployee = async (req, res) => {
@@ -60,9 +67,11 @@ const updateEmployee = async (req, res) => {
         .status(200)
         .json({ message: "Employee updated successfully", employee: employee });
     }
+    console.log(employee + " Updated");
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
+    console.log("Failed to update employee");
   }
 };
 export {
--- index.js
+++ index.js
@@ -5,29 +5,36 @@ import departmentRoutes from "./routes/department.routes.js";
 import statusRoutes from "./routes/status.routes.js";
 import dotenv from "dotenv";
 import authenticateApiKey from "./middleware/auth.middleware.js";
+import cors from "cors";
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected!"))
-  .catch((err) => console.error(err));
-const PORT = process.env.PORT || 3000;
-
 const app = express();
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
 
 app.use("/api/employees", authenticateApiKey, employeeRoutes);
 app.use("/api/departments", authenticateApiKey, departmentRoutes);
 app.use("/api/statuses", authenticateApiKey, statusRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Page  ");
+  res.send("About Page");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
